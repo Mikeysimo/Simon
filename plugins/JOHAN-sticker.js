b196f2ce33d725a13d9fb4d6d455608d44093f0b
Mikeysimo <152785378+Mikeysimo@users.noreply.github.com>
2024-05-02 15:03:26 +0100

    Update JOHAN-sticker.js

--- plugins/JOHAN-sticker.js
+++ plugins/JOHAN-sticker.js
@@ -4,9 +4,9 @@ const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn }) => {
     let res = await axios.get('https://telegra.ph/file/ea03bbc1d2dbd77af8a57.jpg', { responseType: 'arraybuffer' })
     if (!res.data) throw 'Failed to fetch data'
-    let sticker = await conn.sticker(res.data, false, { packname: 'Sticker Pack', author: '@example' })
+    let sticker = await conn.sticker(res.data, false, { packname: '@/مـــايــــكــــي•••   ✌︎⃢☕︎  ', author: 'حرامي 🔪 🔪' })
     await conn.sendMessage(m.chat, sticker, MessageType.sticker, { quoted: m })
 }
 
-handler.command = /^(يوهان)$/i
+handler.command = /^(س)$/i
 module.exports = handler
